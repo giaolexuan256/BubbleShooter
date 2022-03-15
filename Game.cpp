ce790e18de083487f0a08068e10ddd4ac2fbe0e7
giaole256 <giaolexuan256@gmail.com>
2022-03-15 22:57:32 +0700

    First Commit

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by giaol on 3/15/2022.
+//
+
+#include "Game.h"
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Game.h"
+
+int main(int argc, char* args[]) {
+    Game* game = new Game();
+    game->start();
+    return 0;
+}
