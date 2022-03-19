a6649088720d657bface5fa941986b6eab937db9
giaolexuan256 <giaolexuan256@gmail.com>
2022-03-19 21:02:08 +0700

    Update TextureAlpha class

--- src/headers/TextureAlpha.h
+++ src/headers/TextureAlpha.h
@@ -8,59 +8,20 @@
 
 class TextureAlpha {
 public:
-    void free() {
-        if (texture != nullptr) {
-            SDL_DestroyTexture(texture);
-            texture = nullptr;
-            width = 0;
-            height = 0;
-        }
-    }
+    void free();
 
-    void loadFromFile(SDL_Renderer *renderer, const std::string &fileName) {
-        free();
-        SDL_Surface *loadedSurface = IMG_Load(fileName.c_str());
-        if (loadedSurface == nullptr) {
-            printf("Failed to load image %s!\nSDL_image error: %s", fileName.c_str(), IMG_GetError());
-        } else {
-            SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 255, 255, 255));
-            SDL_Texture *newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
-            width = loadedSurface->w;
-            height = loadedSurface->h;
-            SDL_FreeSurface(loadedSurface);
-            texture = newTexture;
-        }
+    void loadFromFile(SDL_Renderer *renderer, const std::string &fileName);
 
-    }
+    void render(SDL_Renderer *renderer, int x, int y, SDL_Rect *sourceRectangle = nullptr);
 
-    void render(SDL_Renderer *renderer, int x, int y, SDL_Rect *sourceRectangle = nullptr) {
-        SDL_Rect destinationRectangle = {x, y, width, height};
-        if (sourceRectangle != nullptr) {
-            destinationRectangle.w = sourceRectangle->w;
-            destinationRectangle.h = sourceRectangle->h;
-        }
+    void renderCenter(SDL_Renderer *renderer, int x, int y, SDL_Rect *sourceRectangle = nullptr);
 
-        SDL_RenderCopy(renderer, texture, sourceRectangle, &destinationRectangle);
-    }
+    void renderExtra(SDL_Renderer *renderer, SDL_Rect *clip, SDL_Rect renderQuad, double angle, SDL_Point *center,
+                     SDL_RendererFlip flip);
 
-    void renderCenter(SDL_Renderer *renderer, int x, int y, SDL_Rect *sourceRectangle = nullptr) {
-        render(renderer, x - (int) getWidth() / 2, y - (int) getHeight() / 2, sourceRectangle);
-    }
+    float getWidth() const;
 
-    void renderExtra(SDL_Renderer *renderer, SDL_Rect *clip, SDL_Rect renderQuad, double angle, SDL_Point *center, SDL_RendererFlip flip) {
-        //Set rendering space and render to screen
-        //Set clip rendering dimensions
-        //Render to screen
-        SDL_RenderCopyEx(renderer, texture, clip, &renderQuad, angle, center, flip);
-    }
-
-    float getWidth() const {
-        return width;
-    }
-
-    float getHeight() const {
-        return height;
-    }
+    float getHeight() const;
 
 
 private:
--- src/sources/TextureAlpha.cpp
+++ src/sources/TextureAlpha.cpp
@@ -3,3 +3,57 @@
 //
 
 #include "../headers/TextureAlpha.h"
+
+void TextureAlpha::free() {
+    if (texture != nullptr) {
+        SDL_DestroyTexture(texture);
+        texture = nullptr;
+        width = 0;
+        height = 0;
+    }
+}
+
+void TextureAlpha::loadFromFile(SDL_Renderer *renderer, const std::string &fileName) {
+    free();
+    SDL_Surface *loadedSurface = IMG_Load(fileName.c_str());
+    if (loadedSurface == nullptr) {
+        printf("Failed to load image %s!\nSDL_image error: %s", fileName.c_str(), IMG_GetError());
+    } else {
+        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 255, 255, 255));
+        SDL_Texture *newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+        width = loadedSurface->w;
+        height = loadedSurface->h;
+        SDL_FreeSurface(loadedSurface);
+        texture = newTexture;
+    }
+
+}
+
+void TextureAlpha::render(SDL_Renderer *renderer, int x, int y, SDL_Rect *sourceRectangle = nullptr) {
+    SDL_Rect destinationRectangle = {x, y, width, height};
+    if (sourceRectangle != nullptr) {
+        destinationRectangle.w = sourceRectangle->w;
+        destinationRectangle.h = sourceRectangle->h;
+    }
+
+    SDL_RenderCopy(renderer, texture, sourceRectangle, &destinationRectangle);
+}
+
+void TextureAlpha::renderCenter(SDL_Renderer *renderer, int x, int y, SDL_Rect *sourceRectangle = nullptr) {
+    render(renderer, x - (int) getWidth() / 2, y - (int) getHeight() / 2, sourceRectangle);
+}
+
+void TextureAlpha::renderExtra(SDL_Renderer *renderer, SDL_Rect *clip, SDL_Rect renderQuad, double angle, SDL_Point *center, SDL_RendererFlip flip) {
+    //Set rendering space and render to screen
+    //Set clip rendering dimensions
+    //Render to screen
+    SDL_RenderCopyEx(renderer, texture, clip, &renderQuad, angle, center, flip);
+}
+
+float TextureAlpha::getWidth() const {
+    return width;
+}
+
+float TextureAlpha::getHeight() const {
+    return height;
+}
