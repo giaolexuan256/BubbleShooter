010f11cc48fbb2aada6dc82b50b045a0c98c20b0
giaolexuan256 <giaolexuan256@gmail.com>
2022-04-04 12:03:18 +0700

    Subcommit

--- src/headers/RandomBubbleColorGenerator.cpp
+++ src/headers/RandomBubbleColorGenerator.cpp
@@ -1,6 +1,6 @@
 #include "RandomBubbleColorGenerator.h"
 
 BubbleColor RandomBubbleColorGenerator::generateRandomBubbleColor() {
-    int randomNumber = RandomNumberGenerator::generateRandomInteger(0, TOTAL_COLORS);
+    int randomNumber = RandomNumberGenerator::generateRandomInteger(0, TOTAL_COLORS - 1);
     return static_cast<BubbleColor>(randomNumber);
 }
